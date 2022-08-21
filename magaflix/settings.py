0823fbdcd0b66f51a29ba6b4818222b6ba59d620
magalercio <magalercio.gui@gmail.com>
2022-08-21 13:04:51 -0300

    Iniciando os HTMLs
    criação base da homepage, base e barra de navegação

--- magaflix/settings.py
+++ magaflix/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'magaflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
