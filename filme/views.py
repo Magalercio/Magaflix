9288b3b1681dcf97e3b08a9fa364d86357d86483
magalercio <magalercio.gui@gmail.com>
2022-09-03 19:30:27 -0300

    Pesquisa
    Funcionalidade da pesquisa

--- filme/views.py
+++ filme/views.py
@@ -41,6 +41,20 @@ class Detalhesfilme (DetailView):
         context = super(Detalhesfilme, self).get_context_data(**kwargs)
         # filtrar a minha tabela de filmes pegando os filmes cuja categoria é igual a categoria do filme da página (object)
         # self.get_object()
-        filmes_relacionados =Filme.objects.filter(categoria=self.get_object().categoria)[0:5]
+        filmes_relacionados =self.model.objects.filter(categoria=self.get_object().categoria)[0:5]
         context['filmes_relacionados'] = filmes_relacionados
         return context
+
+
+class Pesquisafilme(ListView):
+    template_name = "pesquisa.html"
+    model = Filme
+
+    #editando o object_list
+    def get_queryset(self):
+        termo_pesquisa = self.request.GET.get('query')
+        if termo_pesquisa:
+            object_list = self.model.objects.filter(titulo__icontains=termo_pesquisa)
+            return object_list
+        else:
+            return None
